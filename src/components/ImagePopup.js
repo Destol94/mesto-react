319d3905c557bb23ee9017bc7919145c0e58821a
Destol94 <destol94@gmail.com>
2022-10-12 01:17:57 +0700

    10ПР 3 итерация финал

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup(props) {
     <div className={`popup popup_image ${props.card && 'popup_opened'}`} id="popup_image">
       <div className="zoom">
         <figure className="zoom__container">
-          <img className="zoom__img" src={props.card?.link} alt="" />
+          <img className="zoom__img" src={props.card?.link} alt={props.card?.name} />
           <figcaption className="zoom__figcaption">{props.card?.name}</figcaption>
         </figure>
         <button onClick={props.card ? props.onClose : undefined} className="popup__btn-close button"><img className="popup__btn-close-img" alt="кнопка закрыть" src={closeIcon} /></button>
